5c18634d00aa60d228b7124294a83a05c15b8b3d
AnneWLB <anne-kathrin.marx@outlook.com>
2026-04-22 23:49:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.post("/api/check", async (req, res) => {
   const apiKey = process.env.ANTHROPIC_API_KEY;
