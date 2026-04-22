d77e6b5df647d15acf5590bc71ae35400f907f33
AnneWLB <anne-kathrin.marx@outlook.com>
2026-04-22 22:16:22 +0200

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // CORS für alle Origins erlauben
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -20,10 +19,9 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body),
     });
-
     const data = await response.json();
     return res.status(response.status).json(data);
   } catch (err) {
     return res.status(500).json({ error: "Verbindungsfehler: " + err.message });
   }
-}
+};
